ed167cb41546100b7649449b264bfc28d451fecd
Abhinav Kejriwal <ak1@berkeley.edu>
2019-02-13 21:21:29 -0800

    Start Mini Project 2

---
+++ app/src/main/java/ak/com/example/miniproject2/Pokemon.java
@@ -0,0 +1,21 @@
+package ak.com.example.miniproject2;
+
+import java.util.List;
+
+public class Pokemon {
+
+    int hashtag;
+    int Attack;
+    int Defense;
+    String FlavorText;
+    int HP;
+    int Sp_Atk;
+    int Sp_Def;
+    String Species;
+    int Speed;
+    int Total;
+    List<String> Type;
+
+
+
+}
