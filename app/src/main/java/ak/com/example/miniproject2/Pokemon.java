ffa62803f40d4f8e576d5d005f9d298e4cff31b0
Sourish Agarwal <sourish@berkeley.edu>
2019-02-14 01:00:59 -0800

    Added more functionality and JSONObject parsing to Pokemon class.

--- app/src/main/java/ak/com/example/miniproject2/Pokemon.java
+++ app/src/main/java/ak/com/example/miniproject2/Pokemon.java
@@ -1,26 +1,124 @@
 package ak.com.example.miniproject2;
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pokemon {
 
-    int hashtag;
-    int Attack;
-    int Defense;
-    String FlavorText;
-    int HP;
-    int Sp_Atk;
-    int Sp_Def;
-    String Species;
-    int Speed;
-    int Total;
-    List<String> Type;
+    private int number;
+    private int attack;
+    private int defense;
+    private String flavorText;
+    private int hp;
+    private int sp_Atk;
+    private int sp_Def;
+    private String species;
+    private int speed;
+    private int total;
+    private List<String> types;
 
-    String URL;
-    String name;
+    private String URL;
+    private String name;
 
-    public Pokemon(String name) {
-        this.name = name;
-        this.URL = String.format("http://img.pokemondb.net/artwork/%s.jpg", name.toLowerCase());
+    public Pokemon(String n) {
+        name = n;
+        URL = String.format("http://img.pokemondb.net/artwork/%s.jpg", n.toLowerCase());
     }
+
+    public Pokemon(String n, JSONObject pokeAttributes) {
+        //bind name and generate image url
+        name = n;
+        URL = String.format("http://img.pokemondb.net/artwork/%s.jpg", n.toLowerCase());
+
+        try {
+            //bind various attributes
+            number = Integer.parseInt(pokeAttributes.getString("#"));
+            attack = Integer.parseInt(pokeAttributes.getString("Attack"));
+            defense = Integer.parseInt(pokeAttributes.getString("Defense"));
+            flavorText = pokeAttributes.getString("FlavorText");
+            hp = Integer.parseInt(pokeAttributes.getString("HP"));
+            sp_Atk = Integer.parseInt(pokeAttributes.getString("Sp. Atk"));
+            sp_Def = Integer.parseInt(pokeAttributes.getString("Sp. Def"));
+            species = pokeAttributes.getString("Species");
+            speed = Integer.parseInt(pokeAttributes.getString("Speed"));
+            total = Integer.parseInt(pokeAttributes.getString("Total"));
+
+            //Parse types from JSON data
+            types = new ArrayList<>();
+            JSONArray arr = new JSONArray(pokeAttributes.getString("Type"));
+            for (int i = 0; i < arr.length(); i++) {
+                types.add(arr.getString(i));
+            }
+        } catch (JSONException e) {
+            Log.i("Error","JSON data cannot be parsed.");
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public String getFlavorText() {
+        return flavorText;
+    }
+
+    public int getHP() {
+        return hp;
+    }
+
+    public int getSpAtk() {
+        return sp_Atk;
+    }
+
+    public int getSpDef() {
+        return sp_Def;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public List<String> getTypes() {
+        return types;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    /*public static void main(String[] args) throws JSONException {
+        String data = "{\"#\":\"460\",\"Attack\":\"92\",\"Defense\":\"75\",\"FlavorText\":\"\",\"HP\":\"90\",\"Sp. Atk\":\"92\",\"Sp. Def\":\"85\",\"Species\":\"Frost Tree Pok\\u00e9mon\",\"Speed\":\"60\",\"Total\":\"494\",\"Type\":[\"Grass\",\"Ice\"]}";
+        data = data.replaceAll("\n", "\\n");
+        JSONObject attr = new JSONObject(data);
+
+        Pokemon p = new Pokemon("Abomasnow", attr);
+
+        System.out.println(p.getName());
+        System.out.println(p.getURL());
+        System.out.println(p.getDefense());
+        System.out.println(p.getAttack());
+        System.out.println(p.getTypes());
+    }*/
 }
--- app/src/test/java/ak/com/example/miniproject2/UnitTests.java
+++ app/src/test/java/ak/com/example/miniproject2/UnitTests.java
@@ -1,5 +1,7 @@
 package ak.com.example.miniproject2;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,9 +13,47 @@ import static org.junit.Assert.assertEquals;
  */
 public class UnitTests {
     @Test
-    public void testPokemonConstructor() {
-        Pokemon p = new Pokemon("TestPokemon");
-        assertEquals("TestPokemon", p.name);
-        assertEquals("http://img.pokemondb.net/artwork/testpokemon.jpg", p.URL);
+    /** Test URL generation in Pokemon constructor*/
+    public void testPokemonConstructorURL() {
+        Pokemon p1 = new Pokemon("TestPokemon");
+        assertEquals("TestPokemon", p1.getName());
+        assertEquals("http://img.pokemondb.net/artwork/testpokemon.jpg", p1.getURL());
+    }
+
+    @Test
+    /** Test JSON data parsed correctly in Pokemon constructor*/
+    public void testPokemonConstructorJSON() throws JSONException {
+        //instantiate JSON object with pokemon data
+        String data = "{\"#\":\"460\",\"Attack\":\"92\",\"Defense\":\"75\",\"FlavorText\":\"\",\"HP\":\"90\",\"Sp. Atk\":\"92\",\"Sp. Def\":\"85\",\"Species\":\"Frost Tree Pok\\u00e9mon\",\"Speed\":\"60\",\"Total\":\"494\",\"Type\":[\"Grass\",\"Ice\"]}";
+        JSONObject attr = new JSONObject(data);
+
+        Pokemon p2 = new Pokemon("Abomasnow", attr);
+
+        //test name instantiated correctly
+        assertEquals("Abomasnow", p2.getName());
+
+        //test URL instantiated correctly
+        assertEquals("http://img.pokemondb.net/artwork/Abomasnov.jpg", p2.getURL());
+
+        //test number instantiated correctly
+        assertEquals(460, p2.getNumber());
+
+        //test attack instantiated correctly
+        assertEquals(92, p2.getAttack());
+
+//        private int number;
+//        private int attack;
+//        private int defense;
+//        private String flavorText;
+//        private int hp;
+//        private int sp_Atk;
+//        private int sp_Def;
+//        private String species;
+//        private int speed;
+//        private int total;
+//        private List<String> types;
+//
+//        private String URL;
+//        private String name;
     }
 }
